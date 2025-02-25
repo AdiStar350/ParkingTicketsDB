38cdc7136e0b44920c11e52d4f68aab08fd78d5d
Adi Strasser <adistrasser350@gmail.com>
2025-02-25 20:41:02 +0200

    done

--- Q6 - sp_avg_appeal_day.sql
+++ Q6 - sp_avg_appeal_day.sql
@@ -2,7 +2,7 @@
 CREATE PROCEDURE sp_avg_appeal_day
 AS
 BEGIN
-    SELECT AVG(DATEDIFF(DAY, t.ticketDate, a.appealDate))
+    SELECT AVG(DATEDIFF(DAY, t.ticketDate, a.appealDate)) AS [average days for an appeal]
     FROM dbo.appeals a
     JOIN dbo.tickets t
     ON a.ticketId = t.id
